f8608e5634a6df1ee9670c0b9595a579cb276c4d
Ilma Shaharin <shaharinilma96@gmail.com>
2022-10-11 17:58:14 -0400

    Added the files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { CommentCardComponent } from './components/comment-card/comment-card.component';
+import { CommentListComponent } from './components/comment-list/comment-list.component';
+import { ReplyCardComponent } from './components/reply-card/reply-card.component';
+import { ReplyListComponent } from './components/reply-list/reply-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    CommentCardComponent,
+    CommentListComponent,
+    ReplyCardComponent,
+    ReplyListComponent
   ],
   imports: [
     BrowserModule,
